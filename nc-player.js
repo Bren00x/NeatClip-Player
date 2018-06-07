2bea642a8d86d7e6dfeb877f739c77e78c571af1
Bren00x <Bren00x@users.noreply.github.com>
2018-06-07 18:06:34 -0400

    Update nc-player.js

--- nc-player.js
+++ nc-player.js
@@ -1,7 +1,7 @@
 /*
      * JS
      * @bren00x
-     * NeatClip Player v1.0.3
+     * NeatClip Player v1.0.4
      * Built on Plyr.io v2.0.7
      */
     var head = document.getElementsByTagName('head')[0];
@@ -9,7 +9,7 @@
     var custom_style = document.createElement('link');
     custom_style.rel = 'stylesheet';
     custom_style.type = 'text/css';
-    custom_style.href = "https://cdn.jsdelivr.net/gh/bren00x/NeatClip-Player@1.0.3/nc-player.css";
+    custom_style.href = "https://cdn.jsdelivr.net/gh/bren00x/NeatClip-Player@1.0.4/nc-player.css";
     head.appendChild(custom_style);
 
     var plyr_style = document.createElement('link');
@@ -39,38 +39,47 @@
     }
 
     var controls = [
-        "<div class='plyr__controls'>",
-        "<button type='button' data-plyr='play'>",
+	"<div class='plyr__controls'>",
+    "<button type='button' data-plyr='play'>",
         "<svg><use xlink:href='#plyr-play'></use></svg>",
         "<span class='plyr__sr-only'>Play</span>",
-        "</button>",
-        "<button type='button' data-plyr='pause'>",
+    "</button>",
+    "<button type='button' data-plyr='pause'>",
         "<svg><use xlink:href='#plyr-pause'></use></svg>",
         "<span class='plyr__sr-only'>Pause</span>",
-        "</button>",
-        "<span class='plyr__time'>",
+    "</button>",
+    "<span class='plyr__time'>",
         "<span class='plyr__sr-only'>Current time</span>",
         "<span class='plyr__time--current'>00:00</span>",
-        "</span>",
-        "<span class='plyr__progress'>",
+    "</span>",
+    "<span class='plyr__progress'>",
         "<label for='seek{id}' class='plyr__sr-only'>Seek</label>",
         "<input id='seek{id}' class='plyr__progress--seek' type='range' min='0' max='100' step='0.1' value='0' data-plyr='seek'>",
         "<progress class='plyr__progress--played' max='100' value='0' role='presentation'></progress>",
         "<progress class='plyr__progress--buffer' max='100' value='0'>",
-        "<span>0</span>% buffered",
+            "<span>0</span>% buffered",
         "</progress>",
         "<span class='plyr__tooltip'>00:00</span>",
-        "</span>",
-        "<span class='plyr__time'>",
+    "</span>",
+    "<span class='plyr__time'>",
         "<span class='plyr__sr-only'>Duration</span>",
         "<span class='plyr__time--duration'>00:00</span>",
-        "</span>",
-
-        "<button type='button' data-plyr='fullscreen'>",
+    "</span>",
+     "<button class='volume' type='button' data-plyr='mute'>",
+        "<svg class='icon--muted'><use xlink:href='#plyr-muted'></use></svg>",
+        "<svg><use xlink:href='#plyr-volume'></use></svg>",
+        "<span class='plyr__sr-only'>Toggle Mute</span>",
+    "</button>",
+    "<span class='plyr__volume'>",
+        "<label for='volume{id}' class='plyr__sr-only'>Volume</label>",
+        "<input id='volume{id}' class='plyr__volume--input' type='range' min='0' max='10' value='10' data-plyr='volume'>",
+        "<progress class='plyr__volume--display' max='10' value='0' role='presentation'></progress>",
+    "</span>",
+    "<button type='button' data-plyr='fullscreen'>",
         "<svg class='icon--exit-fullscreen'><use xlink:href='#plyr-exit-fullscreen'></use></svg>",
         "<svg><use xlink:href='#plyr-enter-fullscreen'></use></svg>",
         "<span class='plyr__sr-only'>Toggle Fullscreen</span>",
-        "</button>",
+    "</button>",
         html,
         "</div>"
     ].join("");
